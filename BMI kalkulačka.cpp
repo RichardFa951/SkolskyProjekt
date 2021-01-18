a62b3bd43ab3866d90c993d3f591ee8f8c5ac279
DESKTOP-EP9BRN5\richa <richard.faktorr@gmail.com>
2021-01-18 23:08:15 +0100

    ;;;;;;;;;;;;;;;;;;;;;;

--- BMI kalkulačka.cpp
+++ BMI kalkulačka.cpp
@@ -6,16 +6,50 @@ int main()
 	double vaha;
 	double vyska;
 	double BMI;
+	char ano;
 
-	cout << "Zadajte vysku(m) :";
-	cin >> vyska;
+	do {
+		cout << "++++++++++++++++++++++++++++++" << endl;
+		cout << "BMI" << endl;
+		cout << "++++++++++++++++++++++++++++++" << endl;
 
-	cout << "Zadajte hmotnost(kg):";
-	cin >> vaha;
 
-	cout << "BMI:";
-	cin >> BMI;
-	BMI = (vaha*703) / (vyska * vyska);
-	;;
-	return 0;
+		cout << "Zadajte vysku(m) :";
+		cin >> vyska;
+
+		cout << "Zadajte hmotnost(kg):";
+		cin >> vaha;
+
+		BMI = vaha / (vyska * 2);
+		cout << "BMI" << BMI << endl;
+
+		if (BMI < 16);
+		cout << "Kriticky chudy!" << endl;
+
+		else if (BMI > 16, BMI < 17);
+		cout << "Priemerne chudy!" << endl;
+
+		else if (BMI > 17, BMI < 18.5);
+		cout << "Mierne chudy!" << endl;
+
+		else if (BMI > 18.5, BMI < 25);
+		cout << "Normalny!" << endl;
+
+		else if (BMI > 25, BMI < 30);
+		cout << "Nadváda!" << endl;
+
+		else if (BMI > 30, BMI < 35);
+		cout << "Obezita I stupna!" << endl;
+
+		else if (BMI > 35, BMI < 40);
+		cout << "Obezita II stupna!" << endl;
+
+		else if (BMI > 40);
+		cout << "Obezita III stupna!" << endl;
+
+		cout << "Chcete pokracovat? Ak ano stlacte 1 ak nie 2" << endl;
+		cin >> ano;
+	} while (ano == '1');
+
+return 0;
 }
