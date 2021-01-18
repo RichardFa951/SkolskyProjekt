230159bfa770fff401bb999aaca4eb46258c3726
DESKTOP-EP9BRN5\richa <richard.faktorr@gmail.com>
2021-01-18 22:44:18 +0100

    no message

---
+++ BMI kalkulačka.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>;
+using namespace std;
+
+int main()
+{
+	double vaha;
+	double vyska;
+	double BMI;
+
+	cout << "Zadajte vysku(m) :";
+	cin >> vyska;
+
+	cout << "Zadajte hmotnost(kg):";
+	cin >> vaha;
+
+	cout << "BMI:";
+	cin >> BMI;
+	BMI = (vaha*703) / (vyska * vyska);
+	;;
+	return 0;
+}
